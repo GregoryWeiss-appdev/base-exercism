672769d4ea9f60192b770122547e93cd18cb3d6c
Thegegors <gweiss0@chicagobooth.edu>
2021-04-15 20:49:25 +0000

    almost working, looks like last part is messed up with lines

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,8 +1,39 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+class TwelveDays
+  attr_reader :lyrics
+  def self.song
+    intro_start = "On the "
+    intro_end = " day of Christmas my true love gave to me: "
+    part_1 = "and a Partridge in a Pear Tree. \n\n"
+    part_2 = "two Turtle Doves, "
+    part_3 = "three French Hens, "
+    part_4 = "four Calling Birds, "
+    part_5 = "five Gold Rings, "
+    part_6 = "six Geese-a-Laying, "
+    part_7 = "seven Swans-a-Swimming, "
+    part_8 = "eight Maids-a-Milking, "
+    part_9 = "nine Ladies Dancing, "
+    part_10 = "ten Lords-a-Leaping, "
+    part_11 = "eleven Pipers Piping, "
+    part_12 = "twelve Drummers Drumming, "
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+    day_array = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+    song_array = [part_1, part_2, part_3, part_4, part_5, part_6, part_7, part_8, part_9, part_10, part_11, part_12]
 
+    @lyrics = ""
+    counter = 0
+    song_placeholder = ""
+    12.times do |sing|
+      counter += 1
+      if sing == 0
+        @lyrics += intro_start + day_array[sing] + intro_end + part_1[4..-1]
+      else
+        counter.times do |repeat|
+          song_placeholder.prepend(song_array[repeat])
+        end
+        @lyrics += intro_start + day_array[sing] + intro_end + song_placeholder
+        song_placeholder = ""
+      end
+    end
+    return @lyrics
+  end
+end
