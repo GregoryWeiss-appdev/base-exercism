66c4911f2066c17a4241f5ff1a2c0e83aa86e7b9
Thegegors <gweiss0@chicagobooth.edu>
2021-04-15 22:11:36 +0000

    counting up and running

---
+++ ruby/grains/grains.rb
@@ -0,0 +1,24 @@
+=begin
+Write your code for the 'Grains' exercise in this file. Make the tests in
+`grains_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/grains` directory.
+=end
+
+class Grains
+  def self.square(number)
+    if number > 0 && number <= 64
+      square = 2 ** (number - 1)
+    end
+  end
+
+  def self.total
+    total = 0
+    64.times do |run|
+      num = run + 1
+      total += square(num)
+    end
+    total = total
+  end
+end
